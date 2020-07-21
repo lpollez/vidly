40df9893decf900b72d8afba54edf686a148763a
Pollez Laurent <laurent.pollez@gmail.com>
2020-07-21 17:00:44 +0200

    change trown error in startup config file

--- startup/config.js
+++ startup/config.js
@@ -2,7 +2,6 @@ const config = require('config');
 
 module.exports = function () {
   if (!config.get('jwtPrivateKey')) {
-    console.error('FATAL ERROR : jwtPrivateKey is not defined');
-    process.exit(1);
+    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
   }
 };
